7660a4dc69dc1bce417c9d68bc70a6093ac03e24
Codemis <johnathan@missionaldigerati.com>
2012-09-17 18:09:03 -0700

    Getting tweets.

---
+++ app/services/twitter_service.rb
@@ -0,0 +1,36 @@
+# This file is part of Social Micro Blog.
+# 
+# Social Micro Blog is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Social Micro Blog is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see 
+# <http://www.gnu.org/licenses/>.
+#
+# @author Johnathan Pulos <johnathan@missionaldigerati.org>
+# @copyright Copyright 2012 Missional Digerati
+require 'rest_client'
+require 'json'
+
+class TwitterService
+	
+	attr_accessor :credentials
+	
+	def latest(account, max)
+		tweets = Array.new
+		url = "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=#{account}&count=#{max}"
+		request = RestClient.get(url)
+		results = JSON.parse(request)
+		results.each do |tweet|
+			tweets << {id: tweet['id'], content: tweet['text'], provider: 'twitter', user: account, created_at: tweet['created_at']}
+		end
+	end
+
+end
---
+++ spec/services/social_media_spec.rb
@@ -0,0 +1,39 @@
+# This file is part of Social Micro Blog.
+# 
+# Social Micro Blog is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Social Micro Blog is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see 
+# <http://www.gnu.org/licenses/>.
+#
+# @author Johnathan Pulos <johnathan@missionaldigerati.org>
+# @copyright Copyright 2012 Missional Digerati
+#
+# http://infovore.org/archives/2006/08/02/getting-a-class-object-in-ruby-from-a-string-containing-that-classes-name/
+# Initialize Class with String Kernel.const_get('Twitter')
+require_relative "../../app/services/social_media.rb"
+require_relative "../../app/services/twitter_service.rb"
+require 'yaml'
+
+describe SocialMedia do
+	
+	before(:each) do
+		credentials = YAML::load(File.open(File.join(File.dirname(__FILE__), '..', 'config','services.yml')))['services']['twitter']
+		@social_media = SocialMedia.new(TwitterService.new, credentials)
+	end
+	
+	it "should return the latest tweets" do
+		tweets = @social_media.latest('jpulos', 5)
+		tweets.empty?.should be_false
+		tweets.length.should eq(5)
+	end
+	
+end
