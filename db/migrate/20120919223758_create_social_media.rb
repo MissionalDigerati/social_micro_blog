a72824344636068a070c7dffa41f0412cd98ab8d
Codemis <johnathan@missionaldigerati.com>
2012-09-19 15:43:49 -0700

    Added account to social media.

--- db/migrate/20120919223758_create_social_media.rb
+++ db/migrate/20120919223758_create_social_media.rb
@@ -2,6 +2,7 @@ class CreateSocialMedia < ActiveRecord::Migration
   def change
     create_table :social_media do |t|
       t.string :provider
+			t.string :account
       t.integer :provider_id, null: false
       t.text :content
       t.datetime :provider_created_datetime
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(:version => 20120919223758) do
 
   create_table "social_media", :force => true do |t|
     t.string   "provider"
+    t.string   "account"
     t.integer  "provider_id",               :null => false
     t.text     "content"
     t.datetime "provider_created_datetime"
