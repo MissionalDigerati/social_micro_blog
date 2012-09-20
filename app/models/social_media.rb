6849fb889d7525fed6cd52935a6921d127f81a47
Codemis <johnathan@missionaldigerati.com>
2012-09-20 12:45:27 -0700

    Added a method to check if the Social Media post exists.

--- app/models/social_media.rb
+++ app/models/social_media.rb
@@ -1,3 +1,10 @@
 class SocialMedia < ActiveRecord::Base
   attr_accessible :content, :provider, :provider_created_datetime, :provider_id, :account
+
+	 #checks if the post exists
+	#
+	def post_exists?
+		SocialMedia.where("social_media.provider = '#{self.provider}' and social_media.provider_id = #{self.provider_id}").present?
+	end
+
 end
