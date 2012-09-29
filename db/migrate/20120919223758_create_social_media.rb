2981aac9a7912542f70fc35b45af9ecd4a27644f
Codemis <johnathan@missionaldigerati.com>
2012-09-28 18:34:05 -0700

    Changed provider_id to string.

--- db/migrate/20120919223758_create_social_media.rb
+++ db/migrate/20120919223758_create_social_media.rb
@@ -3,7 +3,7 @@ class CreateSocialMedia < ActiveRecord::Migration
     create_table :social_media do |t|
       t.string :provider
 			t.string :account
-      t.integer :provider_id, null: false
+      t.string :provider_id, null: false
       t.text :content
       t.datetime :provider_created_datetime
 
