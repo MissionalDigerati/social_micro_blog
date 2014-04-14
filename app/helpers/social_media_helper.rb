091cace5ca369351cea55f0ae118325de07039df
Codemis <johnathan@missionaldigerati.com>
2014-04-14 16:31:28 -0700

    Fixed the images for all the services.

--- app/helpers/social_media_helper.rb
+++ app/helpers/social_media_helper.rb
@@ -1,2 +1,8 @@
 module SocialMediaHelper
+
+  def avatar(post, url)
+    image = post.avatar
+    link_to image_tag("#{image}", class: 'profile-img'), url, target: "_blank" unless image.nil?
+  end
+
 end
