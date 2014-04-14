f1851dfd8db0298d22663bb9f9cf1500890dc924
Codemis <johnathan@missionaldigerati.com>
2014-04-14 12:14:21 -0700

    added tests for social_avatar.

--- app/models/social_avatar.rb
+++ app/models/social_avatar.rb
@@ -1,3 +1,10 @@
 class SocialAvatar < ActiveRecord::Base
   attr_accessible :account, :avatar_url, :provider, :provider_id
+
+  #checks if the avatar exists
+  #
+  def avatar_exists?
+    SocialAvatar.where("social_avatars.provider = ? and social_avatars.provider_id = ?", self.provider, self.provider_id).present?
+  end
+
 end
