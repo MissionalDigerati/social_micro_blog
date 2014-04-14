d96737aa8aa109e8e4d104f4b3dfc9ff0d9a5281
Codemis <johnathan@missionaldigerati.com>
2014-04-14 12:16:01 -0700

    Added safe sql.

--- app/models/social_media.rb
+++ app/models/social_media.rb
@@ -1,10 +1,10 @@
 class SocialMedia < ActiveRecord::Base
   attr_accessible :content, :provider, :provider_created_datetime, :provider_id, :account
 
-	 #checks if the post exists
+	#checks if the post exists
 	#
 	def post_exists?
-		SocialMedia.where("social_media.provider = '#{self.provider}' and social_media.provider_id = '#{self.provider_id}'").present?
+		SocialMedia.where("social_media.provider = ? and social_media.provider_id = ?", self.provider, self.provider_id).present?
 	end
 	
 	def creation_date
